149b077f10941ca053e54dc6b088f4cd1b762b7d
Geovana <geovanamscardoso183@gmail.com>
2025-05-27 22:08:55 -0300

    Criando o projeto e iniciando função guia

---
+++ global.py
@@ -0,0 +1,48 @@
+'''Função para Guia de sobrevivência em Enchentes'''
+def guia():
+    print("----------------------------------------")
+    print("|                                      |")
+    print("| Guia de Sobrevivência para Enchentes |")
+    print("|                                      |")
+    print("----------------------------------------")
+
+    print("Este Guia visa informar, educar e alertar acerca do que deve ser feito ANTES, DURANTE e APÓS a ocorrência de Enchente e, também, como PREVENIR.\nLeia com atenção, compartilhe essas informações e retorne sempre que houver a necessidade.")
+
+    print("--------------------------------------------------------------------------------------------")
+
+    print("Orientações para PREVENIR enchentes:\n1.Moradores de regiões propensas a inundações devem manter-se informados sobre as condições meteorológicas;\n2.Evite construir em cima e embaixo de barrancos que possam deslizar, carregando sua casa;\n3.Não jogue lixo ou entulho no córrego, para não obstruir a passagem de água, nem em terrenos baldios ou ruas;\n4.Limpe o telhado, calhas, condutores e canaletas para evitar entupimentos;")
+
+    print("--------------------------------------------------------------------------------------------")
+
+    print("Você recebeu a informação que a região onde está sob risco de Enchente, seja por notificação, notícia ou experiências anteriores.\nANTES da enchente você deve:\n\n1.Tenha um lugar previsto, seguro, onde você e sua família possam se alojar no caso de inundação;\n2.Ao primeiro sinal de chuva forte, coloque os móveis, eletrodomésticos e demais objetos em lugares altos;\n3.Coloque documentos e objetos de valor em sacos plásticos bem fechados e em local protegido;\n3.Feche as portas, janelas e o registro de entrada de água;\n4.Desligue aparelhos elétricos e eletrônicos, a chave geral de sua casa e feche os registros de entrada de água e de gás;\n5.Retire todo o lixo e leve-o para áreas não sujeitas a inundações;\n6.Retire os animais de estimação de casa.\n7.Seja solidário: avise seus vizinhos sobre o perigo, no caso de casas construídas em áreas de risco de deslizamentos; avise também, imediatamente,\no Corpo de Bombeiros (193) e à Defesa Civil (199);")
+
+    print("--------------------------------------------------------------------------------------------")
+
+    print("Você já está enfrentando uma enchente na região onde mora.\nDURANTE a enchente você deve:\n1.Proteja a sua vida, a de seus familiares e amigos. Evite contato com as águas de enchentes, elas estão contaminadas e podem provocar doenças;\n2.Se estiver em local seguro, procure não se deslocar; não atravesse ruas alagadas, pois você pode ser arrastado pela água;\n3.Em local alagado, preste atenção a buracos e bueiros sem tampas ou encobertos pela água;\n4.Em caso de ventos muito fortes, cuidado com as quedas de árvores, fios, postes, semáforos, etc;\n5.Utilize calçado, calça comprida e blusa para a proteção do corpo; não use bermuda e não fique sem camisa;\n6.Não deixe crianças brincando na enxurrada ou nas águas dos córregos, pois elas podem ser levadas pela correnteza ou contaminar-se,\ncontraindo doenças graves, como hepatite e leptospirose.\n7.Antes de tudo, salve e proteja vidas. Se precisar retirar algo de sua casa, após a inundação, peça ajuda à \nDefesa Civil ou ao Corpo de Bombeiros nos telefones mencionados acima;\n8.Tente convencer as pessoas que moram em áreas de risco a saírem de casa durante as chuvas.\n9.Evite voltar para casa até as águas baixarem e o caminho estar seguro.Só entre na água se for absolutamente necessário, usando botas de borracha.")
+
+    print("--------------------------------------------------------------------------------------------")
+
+    print("Se você estiver em um carro durante a enchente, diminua a velocidade e mantenha distância do veículo da frente. Procure parar o carro em locais altos e livres de enchentes;\ncertifique-se, por intermédio do rádio, de qual é o melhor itinerário a seguir, a fim de escapar das áreas alagadas; nunca atravesse áreas cobertas pela água;\nse isso não for possível, procure dirigir devagar, mantendo o carro acelerado; evite se aproximar de outro veículo, espere que ele passe totalmente para então seguir em frente;\nSe o nível de água estiver subindo, vá com sua família para um lugar seguro;")
+
+    print("--------------------------------------------------------------------------------------------")
+
+    print("Sua região passou por uma inundação/enchente. APÓS a enchente, você deve:\n1.Beber apenas água filtrada ou fervida;\n2.Não utilizar alimentos que estiveram em contato com a água da inundação;\n3.Ficar atento aos sintomas de doenças, tais como febre, vômito, dor de cabeça ou no corpo (principalmente na “batata da perna”), e diarréia; nesses casos,\nprocure os serviços de saúde;\n4.Chuvas de grande intensidade ou longa duração provocam deslizamentos, principalmente em áreas de risco: fique atento a qualquer sinal;\n5.Não use equipamentos elétricos que tenham sido molhados ou que estiveram em locais inundados, pois há risco de choque elétrico e curto-circuito;\n6.Lave e desinfete os objetos atingidos pela enchente usando uma mistura de um copo de água sanitária para cada balde de 20 litros de água limpa, utilizando luvas e botas.")
+
+#Função Guia está incompleta, falta fazer o sistema para que o usuário tenha opção 
+#de exibir uma seção específica, voltar para o menu inicial do sistema final (que não foi criado), 
+# e sempre que exibir uma seção: dar opção de voltar para o sistema final ou se quer exibir outra
+
+
+'''
+Funções que faltam: Quiz com base nas informações do Guia de Sobrevivência (sistema com alternativas e pontuação)
+
+Consulta de porcentagem de enchentes por região/estado: Perguntar ao usuário qual região ele deseja consultar, após isso, exibir a porcentagem dessa região e perguntar se quer consultar um estado específico da região, se sim, mostrar as opções de estados (sempre com a opção de voltar ao menu inicial) e perguntar qual, e no fim exibir a porcentagem da região (sudeste, nordeste, etc) e do estado escolhido
+
+Outra opção para o Consulta: Quando a região for escolhida e exibida sua porcentagem, perguntar se deseja ver as porcentagens dos estados, se sim, mostrar todos os estados daquela região e seus respectivos dados
+a diferença é que nessa opção todos os estados são exibidos, ao invés do usuário escolher um estado específico
+
+
+Por fim: Falta o sistema final, aquele que terá um menu que será usado para executar as nossas funções de maneira adequada
+'''
+
+
